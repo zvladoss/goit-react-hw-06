f120a037ab0d5c859d910ea3f0b1e41b6ca31559
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2025-04-22 09:48:14 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,7 +16,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["contacts"],
+  whitelist: ["items"],
 };
 
 const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
